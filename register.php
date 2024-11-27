88f71fd83f6c5178d390de1035d14ba2e8c014ef
aidana <aidanaerman61@gmail.com>
2024-11-28 01:49:23 +0500

    added register

---
+++ register.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="styles/style.css">
+    <title>Тіркелу</title>
+</head>
+<body>
+    <h1>Тіркеу формасы</h1>
+    <form method="POST" action="register_action.php">
+        <label>Атыңыз:</label>
+        <input type="text" name="username" required><br>
+        <label>Email:</label>
+        <input type="email" name="email" required><br>
+        <label>Құпиясөз:</label>
+        <input type="password" name="password" required><br>
+        <button type="submit">Тіркелу</button>
+    </form>
+</body>
+</html>
