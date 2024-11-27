d060f1983fcb7f37bb728df3218a7c3eae4c2e90
aidana <aidanaerman61@gmail.com>
2024-11-28 01:43:24 +0500

    new page

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="styles/style.css">
+    <title>Мейрамхана Брондау Жүйесі</title>
+</head>
+<body>
+    <h1>Қош келдіңіз!</h1>
+    <a href="register.php">Тіркелу</a> | <a href="login.php">Кіру</a>
+</body>
+</html>
+    
